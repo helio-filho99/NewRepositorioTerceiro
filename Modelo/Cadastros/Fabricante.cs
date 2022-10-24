c6eb463d16c75c6d23dcba6804c63433d5a2012b
HÉLIO DE OLIVEIRA SILVA FILHO <helio.filho@academico.ifrn.edu.br>
2022-10-24 10:05:21 -0300

    Alterações feitas.

---
+++ Modelo/Cadastros/Fabricante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Modelo.Cadastros
+{
+    public class Fabricante
+    {
+        public long FabricanteId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ Modelo/Tabelas/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using Modelo.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Modelo.Tabelas
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
