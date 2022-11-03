a92927e9e8dd6683dd7986944a5f44834e579863
HÉLIO DE OLIVEIRA SILVA FILHO <helio.filho@academico.ifrn.edu.br>
2022-11-03 11:46:04 -0300

    Alterações feitas.

--- Aula 0505/Controllers/ProdutosController.cs
+++ Aula 0505/Controllers/ProdutosController.cs
@@ -1,25 +1,82 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using System.Net;
+using Modelo.Cadastros;
 using Servico.Cadastros;
 using Servico.Tabelas;
-using Modelo.Cadastros;
 
-namespace PSI2022.Controllers
+namespace aula.Controllers
 {
     public class ProdutosController : Controller
     {
+        private ActionResult ObterVisaoProdutoPorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = produtoServico.ObterProdutoPorId((long)id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // Metodo Privado
+        private void PopularViewBag(Produto produto = null)
+        {
+            if (produto == null)
+            {
+                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(),
+                "CategoriaId", "Nome");
+                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(),
+                "FabricanteId", "Nome");
+            }
+            else
+            {
+                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(),
+                "CategoriaId", "Nome", produto.CategoriaId);
+                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(),
+                "FabricanteId", "Nome", produto.FabricanteId);
+            }
+        }
+
+        // Metodo Privado
+        private ActionResult GravarProduto(Produto produto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    produtoServico.GravarProduto(produto);
+                    return RedirectToAction("Index");
+                }
+                return View(produto);
+            }
+            catch
+            {
+                PopularViewBag(produto);
+                return View(produto);
+            }
+        }
+
+        // private EFContext context = new EFContext(); // Acesso ao contexto (comentado)
         private ProdutoServico produtoServico = new ProdutoServico();
         private CategoriaServico categoriaServico = new CategoriaServico();
         private FabricanteServico fabricanteServico = new FabricanteServico();
-
         // GET: Produtos
         public ActionResult Index()
         {
+            //var produtos = context.Produtos.Include(c => c.Categoria). // Acesso ao contexto
+            // Include(f => f.Fabricante).OrderBy(n => n.Nome); // (comentado)
+            //return View(produtos);
             return View(produtoServico.ObterProdutosClassificadosPorNome());
         }
-
 
         // GET: Produtos/Create
         public ActionResult Create()
@@ -32,13 +89,13 @@ namespace PSI2022.Controllers
         [HttpPost]
         public ActionResult Create(Produto produto)
         {
+            PopularViewBag();
             return GravarProduto(produto);
         }
 
         // GET: Produtos/Edit/5
         public ActionResult Edit(long? id)
         {
-            PopularViewBag(produtoServico.ObterProdutoPorId((long)id));
             return ObterVisaoProdutoPorId(id);
         }
 
@@ -74,55 +131,6 @@ namespace PSI2022.Controllers
             catch
             {
                 return View();
-            }
-        }
-
-        private ActionResult ObterVisaoProdutoPorId(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Produto produto = produtoServico.ObterProdutoPorId((long)id);
-            if (produto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(produto);
-        }
-
-        private void PopularViewBag(Produto produto = null)
-        {
-            if (produto == null)
-            {
-                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(),
-                "CategoriaId", "Nome");
-                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(),
-                "FabricanteId", "Nome");
-            }
-            else
-            {
-                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(),
-                "CategoriaId", "Nome", produto.CategoriaId);
-                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(),
-                "FabricanteId", "Nome", produto.FabricanteId);
-            }
-        }
-
-        private ActionResult GravarProduto(Produto produto)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    produtoServico.GravarProduto(produto);
-                    return RedirectToAction("Index");
-                }
-                return View(produto);
-            }
-            catch
-            {
-                return View(produto);
             }
         }
     }
--- Modelo/Cadastros/Produto.cs
+++ Modelo/Cadastros/Produto.cs
@@ -10,14 +10,17 @@ namespace Modelo.Cadastros
 {
     public class Produto
     {
-        [DisplayName("Id")]
+        [DisplayName("Código")]
         public long? ProdutoId { get; set; }
         [StringLength(100, ErrorMessage = "O nome do produto precisa ter no mínimo 10 caracteres", MinimumLength = 10)]
         [Required(ErrorMessage = "Informe o nome do produto")]
         public string Nome { get; set; }
+
+        [DataType(DataType.Date)]
         [DisplayName("Data de Cadastro")]
         [Required(ErrorMessage = "Informe a data de cadastro do produto")]
         public DateTime? DataCadastro { get; set; }
+
         [DisplayName("Categoria")]
         public long? CategoriaId { get; set; }
         [DisplayName("Fabricante")]
